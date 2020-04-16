9452a89b3a25fd557ab70115e9cdfb02ec4d6eec
Paolo <p.fiaschi@hotmail.it>
2020-04-16 14:47:07 +0200

    ciclare array di oggetti e stampare solo nome e cognome

--- main.js
+++ main.js
@@ -4,38 +4,38 @@
 // -Ciclare su tutti gli studenti e stampare per ognuno nome e cognome;
 // - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-var studente = {
-  "nome": "Mario",
-  "cognome": "Rossi",
-  "eta": "32"
-}
-
-for (var key in studente){
-  console.log(studente[key]);
-  $(".studenti").append('<h1>' + studente[key] + '</h1>');
-}
-
-// var studenti = [{
+// var studente = {
 //   "nome": "Mario",
 //   "cognome": "Rossi",
 //   "eta": "32"
-// },
-// {
-//   "nome": "Gianni",
-//   "cognome": "Boncini",
-//   "eta": "20"
-// },
-// {
-//   "nome": "Alessio",
-//   "cognome": "Bisoli",
-//   "eta": "35"
-// }];
-// console.log(studenti);
-
-// for (var key in studenti){
-//   console.log(studenti[key].nome, studenti[key].cognome);
-//   $(".studenti").append('<h1>' + studenti[key].nome + " " + studenti[key].cognome + " " + '</h1>');
 // }
+//
+// for (var key in studente){
+//   console.log(studente[key]);
+//   $(".studenti").append('<h1>' + studente[key] + '</h1>');
+// }
+
+var studenti = [{
+  "nome": "Mario",
+  "cognome": "Rossi",
+  "eta": "32"
+},
+{
+  "nome": "Gianni",
+  "cognome": "Boncini",
+  "eta": "20"
+},
+{
+  "nome": "Alessio",
+  "cognome": "Bisoli",
+  "eta": "35"
+}];
+console.log(studenti);
+
+for (var key in studenti){
+  console.log(studenti[key].nome, studenti[key].cognome);
+  $(".studenti").append('<h1>' + studenti[key].nome + " " + studenti[key].cognome + " " + '</h1>');
+}
 
 // var nome = prompt("nome");
 // var cognome = prompt("cognome");
